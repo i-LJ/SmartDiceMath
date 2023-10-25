78c2eaea3aac685a261662101f9e1075f74512a6
LJ <1johnlekkas2@gmail.com>
2023-10-25 13:02:30 +0300

    Update smartdicemath.py
    Minor syntax cleanup

--- smartdicemath.py
+++ smartdicemath.py
@@ -5,9 +5,9 @@ getcontext().prec = 40 # This only comes up when converting our large numbers
 # more useful smaller numbers to avoid loss of accuracy.
 
 def roll(dice):
-    for j in range(quantity): # For as many times as we wanna roll dice
+    for _ in range(quantity): # For as many times as we wanna roll dice
         dice.append(random.SystemRandom().randint(1,size)) # "Roll" a number between 1 and our die size
-    for i in range (dtd): # After rolling our numbers
+    for _ in range (dtd): # After rolling our numbers
         dice.remove(min(dice)) # Remove as many of the lowest as specified
     return dice
 
@@ -53,7 +53,6 @@ print("Statistical average: " + str(expectedAverage))
 reps = 10000 # Number of repetitions. More = Slower, but closer to statistical average
 total = 0 # Initializing sum variable
 i = 0 # Initializing iterators
-j = 1
 dice = [] # Temporary for each repetition
 
 while i < reps:
